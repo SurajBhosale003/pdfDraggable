2478c0fb08dba0c3dd9f24a77bd949cc177041ee
SurajBhosale003 <surajedu003@gmail.com>
2024-07-29 10:34:36 +0530

    added PDF render file sepration code

--- src/pdfsb/PdfRenderer.tsx
+++ src/pdfsb/PdfRenderer.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import * as pdfjsLib from 'pdfjs-dist/build/pdf';
 import 'pdfjs-dist/build/pdf.worker.js';
-interface PdfRendererProps {
-    pdfData: string;
-  }
+import { PdfRendererProps } from '../helper/Interface'
   
   const PdfRenderer: React.FC<PdfRendererProps> = ({ pdfData }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
