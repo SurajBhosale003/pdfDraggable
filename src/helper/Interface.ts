d0863289d42221ef914dc36b41a73ac7d5f27eb0
SurajBhosale003 <surajedu003@gmail.com>
2024-07-30 16:54:09 +0530

    added page dependancies In Interface

--- src/helper/Interface.ts
+++ src/helper/Interface.ts
@@ -6,6 +6,7 @@ export interface ComponentData {
     id: number;
     type: 'text' | 'image';
     content?: string;
+    pageNo: number;
     value?: string; 
     position: { top: number; left: number };
     size: { width: number; height: number };
